7bbc7254d105f4fd58024226d687e5c09c6962f0
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2024-03-14 22:47:23 +0530

    fixed bugs

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -1,5 +1,6 @@
 const student = require("../models/student");
 
+module.exports.StudentController = {
 
     getAllStudents:async (req, res) => {
         try {
@@ -11,55 +12,47 @@ const student = require("../models/student");
         }
     },
 
-module.exports.getAllStudents= async (req, res) => {
-    try {
-        const students = await student.getAllStudents();
-        res.status(200).json(students);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-},
+    getAllStudentsNotAssigned :async (req, res) => {
+        try {
+            const students = await student.getAllStudentsNotAssigned();
+            res.status(200).json(students);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
-module.exports.getAllStudentsNotAssigned =async (req, res) => {
-    try {
-        const students = await student.getAllStudentsNotAssigned();
-        res.status(200).json(students);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-},
+    assignMentor: async (req, res) =>  {
+        try {
+            const { studentId, mentorId } = req.body;
+            console.log(studentId, mentorId);
+            const result = await student.assignMentor(studentId, mentorId);
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
-module.exports.assignMentor= async (req, res) =>  {
-    try {
-        const { studentId, mentorId } = req.body;
-        console.log(studentId, mentorId);
-        const result = await student.assignMentor(studentId, mentorId);
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-},
-
-module.exports.removeMentor= async(req,res) => {
-    
-    try {
-    let {studentId,mentorId} = req.body;
-    var data = await student.removeMentor({studentId,mentorId});
+    removeMentor: async(req,res) => {
+       
+        try {
+        let {studentId,mentorId} = req.body;
+        var data = await student.removeMentor({studentId,mentorId});
         res.json(data);
-    }
-    catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-},
+        }
+        catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
-module.exports.updateMarks= async (req, res) => {
-    try {
-        let {vivaMarks, reportSubmissionMarks, projectExplanationMarks, executionMarks, ideationMarks, studentId} = req.body;
-        let data = await student.updateMarksOfStudent({vivaMarks, reportSubmissionMarks, projectExplanationMarks, executionMarks, ideationMarks, studentId});
-        res.status(200).json(data);
+    updateMarks: async (req, res) => {
+        try {
+            let {vivaMarks, reportSubmissionMarks, projectExplanationMarks, executionMarks, ideationMarks, studentId} = req.body;
+            let data = await student.updateMarksOfStudent({vivaMarks, reportSubmissionMarks, projectExplanationMarks, executionMarks, ideationMarks, studentId});
+            res.status(200).json(data);
+        }
+        catch(error){
+            res.status(500).json({ error: error.message });
+        }
+         
     }
-    catch(error){
-        res.status(500).json({ error: error.message });
-    }
-        
-}
+};
