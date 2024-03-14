863027926512f9730e1c84a442755ed72acd3b13
RajendraKumar123 <rajendrakumarvesapogu@gmail.com>
2024-03-14 10:41:02 +0530

    third commit

--- models/student.js
+++ models/student.js
@@ -1,21 +1,38 @@
-class student{
+const db = require("../config/dbConfig");
+module.exports.student={
 
     
-    constructor(){}
-    static async getAllStudents(){
-        const db = require("../config/dbConfig");
+    getAllStudents: async function (){
         const result = await db.pool.query("SELECT * FROM student");
         return result[0];
-    }
-    static async getAllStudentsNotAssigned(){
-        const db = require("../config/dbConfig");
+    },
+    getAllStudentsNotAssigned: async function(){
         const result = await db.pool.query("SELECT * FROM student WHERE assigned_mentor_id is null");
         return result[0];
-    }
-    static async assignMentor(studentId,mentorId){
-        const db = require("../config/dbConfig");
+    },
+    assignMentor: async function(studentId,mentorId){
+        
         const result = await db.pool.query("UPDATE student SET assigned_mentor_id = ? WHERE student_id = ? and assigned_mentor_id is null",[mentorId,studentId]);
         return result[0];
+    },
+    removeMentor: async function(studentId,mentorId){
+
+        const result = await db.pool.query("UPDATE student SET assigned_mentor_id = NULL WHERE student_id = ? and assigned_mentor_id = ?",[studentId, mentorId]);
+        return result;
+    },
+    updateMarks: async function({vivaMarks, reportSubmissionMarks, projectExplanationMarks, executionMarks, ideationMarks, studentId}){
+        const result = await db.pool.query(`
+            UPDATE Students
+            SET 
+                viva_marks = ?,
+                report_submission_marks = ?,
+                project_explanation_marks = ?,
+                execution_marks = ?,
+                ideation_marks = ?
+            WHERE student_id = ?;
+        `, [vivaMarks, reportSubmissionMarks, projectExplanationMarks, executionMarks, ideationMarks, studentId]);
+
+        return result;
     }
 
 
